ff4b605f4fab98ff841b43d1a31e4688f8bb7e84
Ander Linares <ander.linares@outlook.com>
2024-02-29 18:31:40 -0500

    Started store development

--- handler/store.go
+++ handler/store.go
@@ -10,15 +10,51 @@ import (
 	"alc/view/store"
 )
 
-func (h *Handler) HandleStoreShow(c echo.Context) error {
-	items := []model.StoreItem{
-		{
-			Category: "Cargadores",
-			Name:     "SERVIDOR XEON 2224G DELL PowerEdge T40 8|1TB Torre",
-			Price:    300,
-			Slug:     "servidor-xeon-poweredge-1tb",
-			Img:      "/static/img/store1.jpg",
-		},
+var item0 model.StoreItem
+var storeItems []model.StoreItem
+
+func init() {
+	item0 = model.StoreItem{
+		Category:         "Cargadores",
+		Name:             "SERVIDOR XEON 2224G DELL PowerEdge T40 8|1TB Torre",
+		Price:            100,
+		BriefDescription: "El componente fundamental para su pequeña empresa. PowerEdge T40, confiable, eficiente y asequible para brindar soporte a su empresa en crecimiento. Ideal para archivos, impresiones, correos y mensajería.",
+		Description: `Procesador:
+Intel Xeon E-2224G 3.5GHZ (caché de 8 M, 4.70 GHz)
+Memoria Ram:
+8GB DDR4 2666 (1x8GB) 4 Ranuras
+Disco Duro:
+1TB SATA 7200rpm`,
+		Slug:     "servidor-xeon-poweredge-1tb",
+		Img:      "/static/img/store1.jpg",
+		LargeImg: "/static/img/_store1.jpg",
 	}
-	return render(c, http.StatusOK, store.Show(items))
+	for i := 0; i < 9; i++ {
+		storeItems = append(storeItems, item0)
+	}
+}
+
+func (h *Handler) HandleStoreShow(c echo.Context) error {
+	return render(c, http.StatusOK, store.Show(storeItems))
+}
+
+func (h *Handler) HandleStoreItemShow(c echo.Context) error {
+	// Item slug from path `/store/:slug`
+	slug := c.Param("slug")
+
+	var item model.StoreItem
+	found := false
+	for _, i := range storeItems {
+		if i.Slug == slug {
+			item = i
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+
+	return render(c, http.StatusOK, store.ShowItem(item))
 }
--- model/store.go
+++ model/store.go
@@ -1,9 +1,13 @@
 package model
 
 type StoreItem struct {
-	Category string
-	Name     string
-	Price    int // Stored in PEN centimos
-	Slug     string
-	Img      string
+	Category         string
+	Name             string
+	Price            int // Stored in PEN centimos
+	BriefDescription string
+	Description      string
+	Slug             string
+	Img              string
+	LargeImg         string
+	Quantity         int
 }
