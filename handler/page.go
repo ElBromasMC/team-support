df43ae88757032ca9c5b7926b14ead1f3daef6dc
Ander Linares <ander.linares@outlook.com>
2024-03-06 22:44:21 -0500

    Finishing main page

--- handler/page.go
+++ handler/page.go
@@ -5,35 +5,11 @@ import (
 
 	"github.com/labstack/echo/v4"
 
-	"alc/model"
 	"alc/view/page"
 )
 
 func (h *Handler) HandleIndexShow(c echo.Context) error {
-	items := []model.ServiceItem{
-		{
-			Name:            "Soporte para hardware",
-			Description:     "Cambio y reparación de partes para tu equipo",
-			LongDescription: "Cambio y reparación de partes para tu equipo, contamos con certificación de empresa reconocidas para el cambio de partes originales.",
-		},
-		{
-			Name:            "Renting de equipos",
-			Description:     "Alquiler de equipo con soporte por contrato",
-			LongDescription: "Cambio y reparación de partes para tu equipo, contamos con certificación de empresa reconocidas para el cambio de partes originales.",
-		},
-		{
-			Name:            "Proyectos de T.I.",
-			Description:     "Te ayudamos a planificar y realizar tus proyectos de T.I.",
-			LongDescription: "Cambio y reparación de partes para tu equipo, contamos con certificación de empresa reconocidas para el cambio de partes originales.",
-		},
-		{
-			Name:            "Garantías",
-			Description:     "Seguro de vida para tus equipos",
-			LongDescription: "Cambio y reparación de partes para tu equipo, contamos con certificación de empresa reconocidas para el cambio de partes originales.",
-		},
-	}
-
-	return render(c, http.StatusOK, page.Index(items))
+	return render(c, http.StatusOK, page.Index())
 }
 
 func (h *Handler) HandleTicketShow(c echo.Context) error {
--- model/page.go
+++ model/page.go
@@ -1,7 +1,1 @@
 package model
-
-type ServiceItem struct {
-	Name            string
-	Description     string
-	LongDescription string
-}
