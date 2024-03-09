cf1c131c80260a63b6cd0808511befc2f36f5ad5
Ander Linares <ander.linares@outlook.com>
2024-03-09 14:42:15 -0500

    Finished main page

--- view/js/main.js
+++ view/js/main.js
@@ -1,5 +1,7 @@
 import Carousel from "./component/Carousel";
+import CarouselMod from "./component/CarouselMod";
 import ShoppingCart from "./store/ShoppingCart";
 
 customElements.define("my-carousel", Carousel);
+customElements.define("my-carousel-mod", CarouselMod);
 customElements.define("shopping-cart", ShoppingCart)
